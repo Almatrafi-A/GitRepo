0d7353a653c1fcba8ab64608e5336ee94fe328c1
SAF <a.almatrafi@safcsp.org.sa>
2021-04-16 21:54:56 +0300

    Initial Commit

---
+++ GitRepo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitRepo
+//
+//  Created by SAF on 04/09/1442 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
